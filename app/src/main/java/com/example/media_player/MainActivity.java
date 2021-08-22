17f3e869df67286c4c2a65bd5fdd5a1b42722d19
randi_.99 <randimalfdo@gmail.com>
2021-08-22 20:53:14 +0530

    Media Player

---
+++ app/src/main/java/com/example/media_player/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.media_player;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnStart =(Button)this.findViewById(R.id.start);
+        Button btnStop =(Button)this.findViewById(R.id.stop);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+    }
+}
